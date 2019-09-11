978f1c5faf76caedfb8620acd3b4f4a8f0640ae9
JinCheng <JinChengJoker@foxmail.com>
2019-09-11 14:41:13 +0800

    完善测试用例

--- test/index.ts
+++ test/index.ts
@@ -1,35 +1,56 @@
 import EventHub from '../index'
 
-const eventhub = new EventHub()
 
 // 测试 eventhub 是否正确生成
-console.assert(eventhub instanceof Object === true, 'eventhub 应该是一个 object')
+const testEventHub = (message) => {
+  const eventhub = new EventHub()
+  console.assert(eventhub instanceof Object === true, 'eventhub 应该是一个 object')
+  console.log(message)
+}
 
 
-// 测试 on 和 emit 是否可用
-let called = 0
-eventhub.on('test-on-emit', () => {
-  called = 100
-})
-eventhub.emit('test-on-emit')
-console.assert(called === 100, 'called 应该等于 100')
+// 测试 on 和 emit
+const testOnAndEmit = (message) => {
+  const eventhub = new EventHub()
+  let called = false
+  eventhub.on('test-on-emit', () => {
+    called = true
+  })
+  eventhub.emit('test-on-emit')
+  console.assert(called, 'called 应该为 true')
+  console.log(message)
+}
 
 
 // 测试数据传递
-let testdata = 0
-eventhub.on('test-data', (data) => {
-  testdata = data
-})
-eventhub.emit('test-data', 100)
-console.assert(testdata === 100, 'testdata 应该等于 100')
+const testDataTransmission = (message) => {
+  const eventhub = new EventHub()
+  let testdata = 0
+  eventhub.on('test-data', (data) => {
+    testdata = data
+  })
+  eventhub.emit('test-data', 100)
+  console.assert(testdata === 100, 'testdata 应该等于 100')
+  console.log(message)
+}
 
 
 // 测试 off
-let testoff = 0
-let fn1 = () => {
-  testoff = 100
+const testOff = (message) => {
+  const eventhub = new EventHub()
+  let called = false
+  let fn1 = () => {
+    called = true
+  }
+  eventhub.on('test-off', fn1)
+  eventhub.off('test-off', fn1)
+  eventhub.emit('test-off')
+  console.assert(called === false, 'called 应该为 false')
+  console.log(message)
 }
-eventhub.on('test-off', fn1)
-eventhub.off('test-off', fn1)
-eventhub.emit('test-off')
-console.assert(testoff === 0, 'testoff 应该等于 0')
+
+
+testEventHub('测试 eventhub 是否正确生成')
+testOnAndEmit('测试 on 和 emit')
+testDataTransmission('测试数据传递')
+testOff('测试 off')
