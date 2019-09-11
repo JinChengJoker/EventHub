88bdef7dd1e3a575a3a350934a4ec925cfbea2fc
JinCheng <JinChengJoker@foxmail.com>
2019-09-11 15:47:49 +0800

    add type

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 class EventHub {
-  private callstack = {}
-  on(eventname, fn) {
+  private callstack: {[key: string]: Array<(data: any) => void>} = {}
+  on(eventname: string, fn: (data: any) => void) {
     this.callstack[eventname] = this.callstack[eventname] || []
     this.callstack[eventname].push(fn)
   }
-  emit(eventname, data?) {
+  emit(eventname: string, data?: any) {
     if(this.callstack[eventname] === undefined) return
     this.callstack[eventname].forEach(fn => fn(data))
   }
-  off(eventname, fn) {
+  off(eventname: string, fn: (data: any) => void) {
     if(this.callstack[eventname] === undefined || this.callstack[eventname].length === 0) return
     const i = this.callstack[eventname].indexOf(fn)
     if(i === -1) return
